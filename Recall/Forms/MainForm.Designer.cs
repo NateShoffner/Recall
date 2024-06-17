3ed6aad2310c900480ca7514b3ba0479eb1f4e21
Nate Shoffner <nate.shoffner@gmail.com>
2024-06-16 23:29:27 -0400

    Notification tray icon

--- Recall/Forms/MainForm.Designer.cs
+++ Recall/Forms/MainForm.Designer.cs
@@ -28,48 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnEnable = new Button();
-            btnDisable = new Button();
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            btnToggle = new Button();
+            notifyIcon1 = new NotifyIcon(components);
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            toggleMenuItem = new ToolStripMenuItem();
+            lblIcon = new LinkLabel();
+            contextMenuStrip1.SuspendLayout();
             SuspendLayout();
             // 
-            // btnEnable
+            // btnToggle
             // 
-            btnEnable.Location = new Point(12, 12);
-            btnEnable.Name = "btnEnable";
-            btnEnable.Size = new Size(218, 62);
-            btnEnable.TabIndex = 0;
-            btnEnable.Text = "Enable";
-            btnEnable.UseVisualStyleBackColor = true;
-            btnEnable.Click += btnEnable_Click;
+            btnToggle.Location = new Point(12, 12);
+            btnToggle.Name = "btnToggle";
+            btnToggle.Size = new Size(218, 62);
+            btnToggle.TabIndex = 0;
+            btnToggle.Text = "Enable";
+            btnToggle.UseVisualStyleBackColor = true;
+            btnToggle.Click += ToggleService;
             // 
-            // btnDisable
+            // notifyIcon1
             // 
-            btnDisable.Location = new Point(12, 81);
-            btnDisable.Name = "btnDisable";
-            btnDisable.Size = new Size(218, 62);
-            btnDisable.TabIndex = 1;
-            btnDisable.Text = "Disable";
-            btnDisable.UseVisualStyleBackColor = true;
-            btnDisable.Click += btnDisable_Click;
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "Recall";
+            notifyIcon1.Visible = true;
             // 
-            // Form1
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { toggleMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(110, 26);
+            // 
+            // toggleMenuItem
+            // 
+            toggleMenuItem.Name = "toggleMenuItem";
+            toggleMenuItem.Size = new Size(109, 22);
+            toggleMenuItem.Text = "Enable";
+            toggleMenuItem.Click += ToggleService;
+            // 
+            // lblIcon
+            // 
+            lblIcon.AutoSize = true;
+            lblIcon.Location = new Point(61, 111);
+            lblIcon.Name = "lblIcon";
+            lblIcon.Size = new Size(138, 15);
+            lblIcon.TabIndex = 1;
+            lblIcon.TabStop = true;
+            lblIcon.Text = "Icons provided by Icons8";
+            lblIcon.LinkClicked += lblIcon_LinkClicked;
+            // 
+            // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(242, 155);
-            Controls.Add(btnDisable);
-            Controls.Add(btnEnable);
+            Controls.Add(lblIcon);
+            Controls.Add(btnToggle);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
             MinimizeBox = false;
-            Name = "Form1";
+            Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Recall";
+            Load += MainForm_Load;
+            contextMenuStrip1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button btnEnable;
-        private Button btnDisable;
+        private Button btnToggle;
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem toggleMenuItem;
+        private LinkLabel lblIcon;
     }
 }
--- Recall/Forms/MainForm.cs
+++ Recall/Forms/MainForm.cs
@@ -1,3 +1,6 @@
+using Recall.Services;
+using System.Diagnostics;
+
 namespace Recall
 {
     public partial class MainForm : Form
@@ -7,19 +10,36 @@ namespace Recall
         public MainForm()
         {
             InitializeComponent();
+        }
+        private void ToggleService(object sender, EventArgs e)
+        {
+            if (recallService.Enabled)
+            {
+                recallService.Enabled = false;
+                toggleMenuItem.Text = "Enable";
+                btnToggle.Text = "Enable";
+            }
+            else
+            {
+                recallService.Enabled = true;
+                toggleMenuItem.Text = "Disable";
+                btnToggle.Text = "Disable";
+            }
+        }
 
+        private void MainForm_Load(object sender, EventArgs e)
+        {
             string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "recall.db");
             recallService = new RecallService(dbPath);
         }
 
-        private void btnEnable_Click(object sender, EventArgs e)
+        private void lblIcon_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            recallService.Enabled = true;
-        }
-
-        private void btnDisable_Click(object sender, EventArgs e)
-        {
-            recallService.Enabled = false;
+            var psi = new ProcessStartInfo("https://icons8.com/icons/set/eye")
+            {
+                UseShellExecute = true,
+            };
+            Process.Start(psi);
         }
     }
 }
